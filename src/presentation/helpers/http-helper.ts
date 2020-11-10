4e1e713424ac94967858b40661bf0d6107093b75
Rafael Parana <rafael@medprevonline.com>
2020-11-10 07:05:01 -0300

    feat: ensure SignUpController return 400 if an invalid email is provider

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '../controllers/protocols/http'
+import { HttpResponse } from '../protocols/http'
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
