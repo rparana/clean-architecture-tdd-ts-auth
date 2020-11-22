cb43b28e1f5fbe292cc3915a9f3a81e706b55f0c
Rafael Parana <rafael@medprevonline.com>
2020-11-22 19:45:34 -0300

    refactor: group middleware interfacesin a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '../middleware/body-parser'
-import { cors } from '../middleware/cors'
-import { contentType } from '../middleware/content-type'
+import { bodyParser, cors, contentType } from '../middleware'
 
 export default (app: Express): void => {
   app.use(bodyParser)
