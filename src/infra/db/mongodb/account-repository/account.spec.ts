975bdf7a5a8783b72d2f00004ad49865c240779e
Rafael Parana <rafael@medprevonline.com>
2020-11-22 15:57:49 -0300

    refactor: move sut creation to a factory method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -8,8 +8,13 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnet()
   })
+
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
