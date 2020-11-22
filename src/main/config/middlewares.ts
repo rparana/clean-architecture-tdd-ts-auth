b1be8279c4da971bca7312625732f8f717d9dbf0
Rafael Parana <rafael@medprevonline.com>
2020-11-22 17:01:59 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '../middleware/body-parser'
+import { cors } from '../middleware/cors'
+
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
