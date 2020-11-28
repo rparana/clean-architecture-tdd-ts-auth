b55ea93b07380b5eb8f76a8a209298c0b8de9756
Rafael Parana <rafael@medprevonline.com>
2020-11-27 21:48:51 -0300

    refactor: move makeSut to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,9 +3,18 @@ import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { HttpRequest } from '../../protocols'
 
+interface SutTypes {
+  sut: LoginController
+}
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
+}
 describe('Login Controller', () => {
-  test('Shoud return 400 if no email is provider', async () => {
-    const sut = new LoginController()
+  test('Shoud return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest: HttpRequest = {
       body: {
         password: 'any_password'
@@ -15,8 +24,8 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
-  test('Shoud return 400 if no password is provider', async () => {
-    const sut = new LoginController()
+  test('Shoud return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest: HttpRequest = {
       body: {
         email: 'any_email@mail.com'
