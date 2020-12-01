a67779e768207efcbe9d539383e6ea435668bce3
Rafael Parana <rafael@medprevonline.com>
2020-11-30 22:34:11 -0300

    test: ensure RequiredFieldValidation return a MissingParamsError if validation fails

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('Required Field Validation', () => {
+  test('Should return a MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+})
