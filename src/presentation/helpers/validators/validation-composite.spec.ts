70b965aba2d01203326e18d6c4bba1a1f97643bf
Rafael Parana <rafael@medprevonline.com>
2020-11-30 23:12:17 -0300

    test: ensure ValidationComposite not return if validations succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -39,4 +39,10 @@ describe('ValidationComposite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
