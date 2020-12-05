85bcf7e76a34bfaf920b9180a012630a8d1f6c2b
Rafael Parana <rafael@medprevonline.com>
2020-12-04 21:24:27 -0300

    feat: ensure DbAuthentication call HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,15 +1,21 @@
+import { HashComparer } from 'data/protocols/criptography/hash-comparer'
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly hashComparer: HashComparer
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparer = hashComparer
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email)
+    const userAccount = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (userAccount) {
+      await this.hashComparer.compare(authentication.password, userAccount.password)
+    }
     return null
   }
 }
