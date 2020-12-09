c1b34286ade52d7b8d94dd95bf68a7400c8cb087
Rafael Parana <rafael@medprevonline.com>
2020-12-08 22:22:16 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,11 +1,11 @@
-import { Validation } from '../../../presentation/protocols/validation'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { RequiredFieldValidation } from '../../../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../../../presentation/helpers/validators/validation-composite'
 import { makeLoginValidation } from './login-validation-factory'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { EmailValidation } from '../../../../presentation/helpers/validators/email-validation'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,11 @@
+import { makeSignupValidation } from './signup-validation-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+import { Controller } from '../../../../presentation/protocols'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
+import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
+
+export const makeSignupController = (): Controller => {
+  const signupController = new SignUpController(makeDbAddAccount(), makeSignupValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(signupController)
+}
