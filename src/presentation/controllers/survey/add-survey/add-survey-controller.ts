9166545b370901ae87b81c1d9a480a941cab65f8
Rafael Parana <rafael@medprevonline.com>
2020-12-10 21:42:51 -0300

    feat: ensure AddSurveyProtocol return badRequest error if Validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,10 +1,14 @@
 import { Controller, HttpRequest, Validation } from './add-survey-protocols'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 export class AddSurveyController implements Controller {
   constructor (private readonly validation: Validation) { }
 
   async handle (httpRequest: HttpRequest): Promise<any> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return null
   }
 }
