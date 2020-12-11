fa088576da2ce2bfd3bc50961b8cc4bf3e9f8dd9
Rafael Parana <rafael@medprevonline.com>
2020-12-10 21:43:07 -0300

    test: ensure AddSurveyProtocol return badRequest error if Validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,5 +1,7 @@
 import { HttpRequest, Validation } from './add-survey-protocols'
+import { MissingParamError } from '../../../errors'
 import { AddSurveyController } from './add-survey-controller'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 interface SurveyModel {
   question: string
@@ -19,7 +21,7 @@ const makeFakeSurveyModel = (): SurveyModel => ({
   }]
 })
 
-const makeFakeHttpRequest = (): HttpRequest => {
+const makeFakeRequest = (): HttpRequest => {
   return { body: makeFakeSurveyModel() }
 }
 const makeValidationStub = (): Validation => {
@@ -48,8 +50,15 @@ describe('AddSurvey Controller', () => {
   test('Should calls Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const httpRequest = makeFakeHttpRequest()
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
