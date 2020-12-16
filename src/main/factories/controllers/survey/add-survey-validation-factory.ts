a789af7b9f661e0bebdcf49a97fa8431e5a8db15
Rafael Parana <rafael@medprevonline.com>
2020-12-15 21:06:47 -0300

    feat: add AddSurveyValidationFactory

---
+++ src/main/factories/controllers/survey/add-survey-validation-factory.ts
@@ -0,0 +1,12 @@
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  const requiredFields = ['question', 'answers']
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  return new ValidationComposite(validations)
+}
