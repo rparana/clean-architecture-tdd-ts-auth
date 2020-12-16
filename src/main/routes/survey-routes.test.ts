a202c77042e99b10c77643529ee78db9c9d2cbec
Rafael Parana <rafael@medprevonline.com>
2020-12-15 21:09:51 -0300

    test: add AddSurveyComposite

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /survey', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/survey')
+        .send({
+          question: 'Question',
+          answers: [{
+            image: 'https://image.name.com',
+            answer: 'Answer 1'
+          },
+          {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
