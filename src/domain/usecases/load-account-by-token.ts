e15833c217ac4bc238c8f2401c724212023c8513
Rafael Parana <rafael@medprevonline.com>
2020-12-15 22:17:56 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct Params

---
+++ src/domain/usecases/load-account-by-token.ts
@@ -0,0 +1,4 @@
+import { AccountModel } from '../models/account'
+export interface LoadAccountByToken {
+  load: (accessToken: string, role?: string) => Promise<AccountModel>
+}
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,15 @@
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { Middleware, HttpRequest, HttpResponse } from '../protocols'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (httpRequest.headers) {
+      await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
+    }
     return new Promise(resolve => resolve(forbidden(new AccessDeniedError())))
   }
 }
