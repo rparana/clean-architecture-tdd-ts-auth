9e18938226d826d2ce42668e1ddc23358bd22a81
Rafael Parana <rafael@medprevonline.com>
2020-12-15 21:08:23 -0300

    test: ensure AddSurveyValidationFactory call ValidationComposite with correct validations

---
+++ src/main/factories/controllers/survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { Validation } from '../../../../presentation/protocols/validation'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('AddSurveyValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    const requiredFields = ['question', 'answers']
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
