23b51937bc8826e2b1931a1a70953a25072e1f62
Rafael Parana <rafael@medprevonline.com>
2020-12-16 23:03:31 -0300

    feat: ensure DbLoadAccountByToken call LoadAccountByToken with correct token

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -10,7 +10,7 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   async load (accessToken: string, role?: string): Promise<AccountModel> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      const account = await this.loadAccountByTokenRepository.load(accessToken)
+      const account = await this.loadAccountByTokenRepository.loadByToken(token)
       return account
     }
     return null
