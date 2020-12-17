8f4845a3be53509ee5583863ddd598979ae495f5
Rafael Parana <rafael@medprevonline.com>
2020-12-16 22:13:58 -0300

    feat: ensure DbLoadAccountByToken call LoadAccountByToken with correct token

---
+++ src/data/usecases/middleware/db-load-account-by-token-protocols.ts
@@ -0,0 +1,2 @@
+export * from '../../../domain/models/account'
+export * from '../../../domain/usecases/load-account-by-token'
---
+++ src/data/usecases/middleware/db-load-account-by-token.ts
@@ -0,0 +1,12 @@
+import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
+import { AccountModel } from '../login/add-account/db-add-account-protocols'
+import { LoadAccountByToken } from './db-load-account-by-token-protocols'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository) {}
+
+  async load (token: string): Promise<AccountModel> {
+    await this.loadAccountByTokenRepository.loadByToken(token)
+    return null
+  }
+}
