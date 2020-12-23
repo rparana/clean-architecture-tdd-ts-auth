8b1031621d85d252afda011fbe11c4e4193afccf
Rafael Parana <rafael@medprevonline.com>
2020-12-22 22:21:56 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -93,10 +93,21 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
-    test('Should return an null if loadByEmail fails', async () => {
+    test('Should return an account on loadByToken with role success', async () => {
       const sut = makeSut()
-      const account = await sut.loadByEmail('any_email@mail.com')
-      expect(account).toBeFalsy()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
   })
 })
