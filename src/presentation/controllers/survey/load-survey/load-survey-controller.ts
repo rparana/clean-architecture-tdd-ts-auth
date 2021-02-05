4a1817a1eb984c4c24429029d88ec83646027d0a
Rafael Parana <rafael@medprevonline.com>
2021-02-04 22:11:12 -0300

    feat: ensure LoadSurveyController returns 200 if load succeeds

--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,11 +1,15 @@
 import { LoadSurvey } from '@/domain/usecases/load-survey'
-import { noContent } from '@/presentation/helpers/http/http-helper'
+import { noContent, ok } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from './load-survey-protocols'
 
 export class LoadSurveyController implements Controller {
   constructor (private readonly loadSurvey: LoadSurvey) { }
 
   async handle (_httpRequest: HttpRequest): Promise<HttpResponse> {
-    return noContent()
+    const surveys = await this.loadSurvey.load()
+    if (!surveys) {
+      return noContent()
+    }
+    return ok(surveys)
   }
 }
