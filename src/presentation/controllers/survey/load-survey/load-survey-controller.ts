90fc733c4bf43356b48a3714604750d1c1cb75be
Rafael Parana <rafael@medprevonline.com>
2021-02-04 22:16:20 -0300

    feat: ensure LoadSurveyController returns 500 if LoadSurvey throws

--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,15 +1,19 @@
 import { LoadSurvey } from '@/domain/usecases/load-survey'
-import { noContent, ok } from '@/presentation/helpers/http/http-helper'
+import { noContent, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from './load-survey-protocols'
 
 export class LoadSurveyController implements Controller {
   constructor (private readonly loadSurvey: LoadSurvey) { }
 
   async handle (_httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurvey.load()
-    if (!surveys) {
-      return noContent()
+    try {
+      const surveys = await this.loadSurvey.load()
+      if (!surveys) {
+        return noContent()
+      }
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
     }
-    return ok(surveys)
   }
 }
