b9c7f620d50534d85e6cd5125b6e6d80a01c945c
Rafael Parana <rafael@medprevonline.com>
2021-02-15 21:11:20 -0300

    test: ensure SurveyMongoRepository return empty list if there are no surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -72,5 +72,11 @@ describe('Survey Mongo Repository', () => {
       expect(surveys[0].question).toBe('any_question')
       expect(surveys[1].question).toBe('other_question')
     })
+
+    test('Should load surveys return empty list', async () => {
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(0)
+    })
   })
 })
