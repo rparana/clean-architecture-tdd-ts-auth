7132e3ca3e0552a51e2c59e77fbb97c5a33ff2ef
Rafael Parana <rafael@medprevonline.com>
2021-02-15 21:09:01 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -4,38 +4,73 @@ import { SurveyMongoRepository } from './survey-mongo-repository'
 
 let surveyCollection: Collection
 
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  surveyCollection = await MongoHelper.getCollection('surveys')
+  await surveyCollection.deleteMany({})
+})
+
 describe('Survey Mongo Repository', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
+  describe('add Survey', () => {
+    test('Should add a survey success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
   })
 
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
-
-  beforeEach(async () => {
-    surveyCollection = await MongoHelper.getCollection('surveys')
-    await surveyCollection.deleteMany({})
-  })
-
-  const makeSut = (): SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
-
-  test('Should add a survey success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
+  describe('load Surveys', () => {
+    test('Should load surveys success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer'
+        }],
+        date: new Date()
       },
       {
-        answer: 'any_answer'
-      }],
-      date: new Date()
+        question: 'other_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }
+      ])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
