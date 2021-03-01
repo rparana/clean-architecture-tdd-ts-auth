40e017a1086c502ba24933052e025c731a31f3d3
Rafael Parana <rafael@medprevonline.com>
2021-02-28 22:05:40 -0300

    feat: ensure SaveSurveyResultController return 403 if LoadSurveyById returns null

--- src/presentation/controllers/save-survey-result-controller.ts
+++ src/presentation/controllers/save-survey-result-controller.ts
@@ -1,9 +1,15 @@
-import { Controller, HttpRequest, Validation } from '@/presentation/protocols'
+import { LoadSurveyById } from '@/domain/usecases'
+import { Controller, HttpRequest } from '@/presentation/protocols'
+import { InvalidParamError } from '../errors'
+import { forbidden } from '../helpers/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (private readonly loadSurveyById: LoadSurveyById) {}
   async handle (httpRequest: HttpRequest): Promise<any> {
-    this.validation.validate(httpRequest.body)
+    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
     return null
   }
 }
