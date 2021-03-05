c3b611793a6c25239de12355450a484b87b90f57
Rafael Parana <paranafael@yahoo.com.br>
2021-03-04 23:20:38 -0300

    feat: ensure save survey results return 200 on success

--- src/data/protocols/db/survey/index.ts
+++ src/data/protocols/db/survey/index.ts
@@ -1,4 +1,3 @@
 export * from './add-survey-repository'
 export * from './load-survey-by-id-repository'
 export * from './load-survey-repository'
-export * from './save-survey-result-repository'
--- src/infra/db/mongodb/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey-mongo-repository.ts
@@ -2,6 +2,7 @@ import { AddSurveyRepository, LoadSurveyByIdRepository, LoadSurveysRepository } 
 import { AddSurveyModel } from '@/domain/usecases'
 import { SurveyModel } from '@/domain/models'
 import { MongoHelper } from '.'
+import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
@@ -17,7 +18,7 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
 
   async loadById (id: string): Promise<SurveyModel> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const survey = await surveyCollection.findOne({ _id: id })
+    const survey = await surveyCollection.findOne({ _id: new ObjectId(id) })
     return survey && MongoHelper.map(survey)
   }
 }
