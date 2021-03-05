7a925064322d636c3b2ec014361988f9ca8f5027
Rafael Parana <paranafael@yahoo.com.br>
2021-03-04 22:34:33 -0300

    test: ensure returns 403 if no user is authenticated

---
+++ tests/main/routes/survey-results-routes.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('PUT /surveys/:surveiId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(403)
+    })
+  })
+})
