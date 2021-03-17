ffa22bd002ae2645b35b71f87598099dcd265038
Rafael Parana <paranafael@yahoo.com.br>
2021-03-16 22:55:52 -0300

    feat: ensure DbLoadSurveyResults return SurveyResults on success

--- src/data/usecases/db-load-survey-results.ts
+++ src/data/usecases/db-load-survey-results.ts
@@ -4,7 +4,7 @@ import { LoadSurveyResultRepository } from '../protocols'
 export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
   async load (surveyId: string, accountId: string): Promise<LoadSurveyResult.Result> {
-    await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId)
-    return null
+    const surveyResults = await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId)
+    return surveyResults
   }
 }
