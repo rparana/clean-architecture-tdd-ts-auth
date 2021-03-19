f2a636ade39a9d32052d42b7790984cb4f90d0b7
Rafael Parana <paranafael@yahoo.com.br>
2021-03-19 08:45:10 -0300

    feat: ensure DbLoadAccountByToken return null if decrypter throws

--- tests/data/usecases/db-load-account-by-token.spec.ts
+++ tests/data/usecases/db-load-account-by-token.spec.ts
@@ -41,11 +41,11 @@ describe('DbLoadAccountByToken Usecase', () => {
     expect(account).toBeNull()
   })
 
-  test('Should throw if Decrypter throws', async () => {
+  test('Should return null if Decrypter throws', async () => {
     const { sut, decrypterSpy } = makeSut()
     jest.spyOn(decrypterSpy, 'decrypt').mockImplementationOnce(throwError)
-    const promise = sut.load(token, role)
-    await expect(promise).rejects.toThrow()
+    const promise = await sut.load(token, role)
+    expect(promise).toBeNull()
   })
 
   test('Should call LoadAccountByTokenRepository with correct value', async () => {
