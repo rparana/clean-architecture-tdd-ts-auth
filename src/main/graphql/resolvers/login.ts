8d81e73a9c4344f4571284153ffc11752d23f845
Rafael Parana <rafael@medprevonline.com>
2021-03-20 16:20:50 -0300

    feat: make login resolver use login controller

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,9 @@
+import { adaptResolver } from '@/main/adapters'
+import { makeLoginController } from '@/main/factories/controllers'
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+
 export default {
   Query: {
-    login () {
-      return {
-        accessToken: 'any token',
-        name: 'any name'
-      }
-    }
+    login: async (_parent: any, args: any) => adaptResolver(makeLogControllerDecorator(makeLoginController()), args)
   }
 }
