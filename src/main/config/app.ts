b05d706ae974a831dbc797b795b0390ed3ca7ee7
Rafael Parana <rafael@medprevonline.com>
2021-03-20 15:59:42 -0300

    feat: setupr ApolloServer

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupApolloServer from './apollo-server'
 import setupRoutes from './routes'
 
 const app = express()
+setupApolloServer(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
